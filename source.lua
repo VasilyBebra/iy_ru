4eeae1445354b3846c343a40a12a72b72af29974
VasilyBebra <120628968+VasilyBebra@users.noreply.github.com>
2025-12-14 20:23:55 +0500

    Update source.lua
    фикс queueteleport (загружало оригинал вместо русификатора)

--- source.lua
+++ source.lua
@@ -6489,7 +6489,7 @@ local TeleportCheck = false
 Players.LocalPlayer.OnTeleport:Connect(function(State)
 	if KeepInfYield and (not TeleportCheck) and queueteleport then
 		TeleportCheck = true
-		queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()")
+		queueteleport("loadstring(game:HttpGet('https://github.com/VasilyBebra/iy_ru/raw/refs/heads/main/source.lua'))()")
 	end
 end)
 
